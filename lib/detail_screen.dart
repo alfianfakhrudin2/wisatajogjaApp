1f7fd2d513c2013e5e2e17f71d9f3814d53a940d
Alfian <alfianfakhrudin11@gmail.com>
2024-02-21 03:24:56 +0700

    adding border radius for image

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -101,17 +101,39 @@ class FirstScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Image.network(
-                  'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg')
+              SizedBox(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                              'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                        ))
+                  ],
+                ),
+              )
             ]),
-      )
-          // child: ElevatedButton(
-          //   onPressed: () {
-          //     Navigator.push(context);
-          //   },
-          //   child: const Text('Go to Second Screen'),
-          // ),
-          ),
+      )),
     );
   }
 }
